63f88c0aeea2b8c0e9eb5390b588109e390f4af4
Simon Knopp <simon.knopp@pg.canterbury.ac.nz>
2011-12-02 11:40:25 +1300

    Add setup.py to register as a pygments plugin.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name = "pygments-bitbake",
+    version = "1.0.0",
+    license = "BSD",
+    author = "Simon Knopp",
+    author_email = "simon.knopp@pg.canterbury.ac.nz",
+    url = "https://github.com/sijk/pygments-bitbake",
+    description = "Pygments lexer for BitBake files.",
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Text Processing :: Markup",
+    ],
+
+    install_requires = ["pygments"],
+    entry_points = {'pygments.lexers': 'bitbake = bitbake:BitbakeLexer'},
+
+    py_modules = ["bitbake"],
+)
